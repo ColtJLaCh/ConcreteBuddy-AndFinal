6b156e8187b47612f3c7d6d7f494850985150b2c
ColtJLaCh <CL51@myscc.ca>
2021-11-24 18:31:38 -0500

    Touched up tips fragment

--- app/src/main/java/coltonlachance/com/concretecalculator/CustomViewPageAdapter.java
+++ app/src/main/java/coltonlachance/com/concretecalculator/CustomViewPageAdapter.java
@@ -17,9 +17,9 @@ public class CustomViewPageAdapter extends FragmentStateAdapter {
 
     public Fragment createFragment(int position) {
         switch(position) {
-            case 0: return VPTipsFragment.newInstance(position+1,"Check The Weather!",R.drawable.normalbagofconcrete,"Always check the weather ahead of time to see when the best days to set your concrete would be.\n Remember!\nWhether or not your concrete sets properly is mostly based on the weather!");
-            case 1: return VPTipsFragment.newInstance(position+1,"Clear out the bubbles!",R.drawable.normalbagofconcrete,"After pouring your concrete, grab yourself a rake or something similar and push and swish the concrete around to get out any air bubbles that would otherwise weaken the after setting.");
-            case 2: return VPTipsFragment.newInstance(position+1,"Get multiple people for the job!",R.drawable.normalbagofconcrete,"It may seem easier to just do it all yourself, but having multiple people there to help not only speeds up the process, but makes it more likely you get your concrete set properly! Especially when using ready-mix concrete.");
+            case 0: return VPTipsFragment.newInstance(position+1,"Check The Weather!",R.drawable.rainoutside,"Always check the weather ahead of time to see when the best days to set your concrete would be.\n\nRemember!\n\nWhether or not your concrete sets properly is mostly based on the weather!");
+            case 1: return VPTipsFragment.newInstance(position+1,"Clear out the bubbles!",R.drawable.rakeconcrete,"After pouring your concrete,\ngrab yourself a rake or something similar.\n\nSwish the concrete around to get out any air bubbles that would otherwise weaken the after setting!");
+            case 2: return VPTipsFragment.newInstance(position+1,"Get multiple people for the job!",R.drawable.construction,"Although it may seem easier to just do it all yourself...\n\nHaving multiple people there to assist you will speed up the progress, \n\nMakes it more likely you get your concrete set properly!\n\nThese are especially when using ready-mix concrete.");
             default: return VPTipsFragment.newInstance(0,"NULL",R.drawable.normalbagofconcrete,"NULL");
         }
     }
