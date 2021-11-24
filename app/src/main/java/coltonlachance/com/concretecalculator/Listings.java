43a05ab77d5704a3fe9755e40ab12afc71189c1b
ColtJLaCh <CL51@myscc.ca>
2021-11-24 18:02:57 -0500

    Added recycler view adapter with listings from Lab9

---
+++ app/src/main/java/coltonlachance/com/concretecalculator/Listings.java
@@ -0,0 +1,38 @@
+package coltonlachance.com.concretecalculator;
+
+public class Listings {
+    private String name;
+    private String definition;
+    private String price;
+
+    public Listings(String name, String definition, String price) {
+        this.name = name;
+        this.definition = definition;
+        this.price = price;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDefinition() {
+        return definition;
+    }
+
+    public void setDefinition(String definition) {
+        this.definition = definition;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+}
