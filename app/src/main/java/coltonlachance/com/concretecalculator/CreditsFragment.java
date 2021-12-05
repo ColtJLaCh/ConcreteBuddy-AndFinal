f614f6deeac45300a3009f6fe079880517cbc6bb
ColtJLaCh <CL51@myscc.ca>
2021-12-05 15:57:30 -0500

    #7 #18 create credits layout

---
+++ app/src/main/java/coltonlachance/com/concretecalculator/CreditsFragment.java
@@ -0,0 +1,124 @@
+package coltonlachance.com.concretecalculator;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link CreditsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class CreditsFragment extends Fragment {
+
+    public TextView DataTypeDesc;
+    public ListView list;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public CreditsFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment CreditsFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static CreditsFragment newInstance(String param1, String param2) {
+        CreditsFragment fragment = new CreditsFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_credits, container, false);
+        DataTypeDesc = (TextView) view.findViewById(R.id.DataTypeDesc);
+        list = (ListView) view.findViewById(R.id.dataTypeList);
+
+        ArrayList<DataTypeItem> dataTypeList = new ArrayList<DataTypeItem>();
+
+        dataTypeList.add(new DataTypeItem("Image on homepage","Created using Paint.net."));
+        dataTypeList.add(new DataTypeItem("TIP #1 Image", "From https://pixabay.com/photos/dark-rain-windows-glass-windows-1850684/, edited."));
+        dataTypeList.add(new DataTypeItem("TIP #2 Image", "From https://pixabay.com/photos/baseball-field-sports-field-1495657/, edited."));
+        dataTypeList.add(new DataTypeItem("TIP #3 Image", "From https://pixabay.com/photos/construction-site-construction-worker-3279650/, edited."));
+        dataTypeList.add(new DataTypeItem("Concrete Buddy App", "Created solely by Colton LaChance."));
+
+        //ArrayAdapter adapter = new ArrayAdapter(getContext(), android.R.layout.simple_list_item_1,dataTypeList);
+        CustomAdapter adapter = new CustomAdapter(getContext(), dataTypeList);
+
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                DataTypeDesc.setText(((DataTypeItem)list.getItemAtPosition(position)).getDesc());
+            }
+        });
+
+        return view;
+    }
+
+    public class CustomAdapter extends ArrayAdapter<DataTypeItem> {
+        public CustomAdapter(Context context, ArrayList<DataTypeItem> items) {
+            super(context,0,items);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            DataTypeItem item = getItem(position);
+
+            /*
+
+            ---------------COMMENT AND UNCOMMENT HERE!-----------------
+
+            */
+            if (convertView == null) {
+                //convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_view,parent,false);
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.my_item_view,parent,false);
+            }
+
+            TextView name = (TextView) convertView.findViewById(R.id.name);
+            name.setText(item.getName());
+            return convertView;
+        }
+    }
+}
--- app/src/main/java/coltonlachance/com/concretecalculator/MainActivity.java
+++ app/src/main/java/coltonlachance/com/concretecalculator/MainActivity.java
@@ -69,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_calculator, R.id.nav_tips, R.id.nav_listings,R.id.nav_contact,R.id.nav_home)
+                R.id.nav_calculator, R.id.nav_tips, R.id.nav_listings,R.id.nav_contact,R.id.nav_home,R.id.nav_credits)
                 .setDrawerLayout(drawer)
                 .build();
         navController = Navigation.findNavController(this, R.id.nav_host_fragment);
